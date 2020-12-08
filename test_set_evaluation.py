aedd5fdc55df0d2c513f41d7838ca7bdb134d1ea
Jessica Mattick <jmatty1661@gmail.com>
2020-12-07 23:54:15 -0600

    add scripts for final model evaluation

--- test_set_evaluation.py
+++ test_set_evaluation.py
@@ -8,12 +8,18 @@ from sklearn.metrics import log_loss
 import matplotlib.pyplot as plt
 from sklearn.dummy import DummyClassifier
 
+#train data
 data = pd.read_csv('data/train/X_train_encoded.csv', index_col=0)
 data_pca = pd.read_csv('data/train/X_train_gene_pca.csv', index_col=0)
 data_pca_all = pd.read_csv('data/train/X_train_gene_viability_pca.csv', index_col=0)
 targets = pd.read_csv('data/train/y_train.csv', index_col=0)
 
+
 #test data
+test_data = pd.read_csv('data/test/X_test_encoded.csv', index_col=0)
+test_data_pca = pd.read_csv('data/test/X_test_gene_pca.csv', index_col=0)
+test_data_pca_all = pd.read_csv('data/test/X_test_gene_viability_pca.csv', index_col=0)
+test_targets = pd.read_csv('data/test/y_test.csv', index_col=0)
 
 
 
@@ -35,18 +41,16 @@ results_dum = []
 
 
 # run model on encoded data
-X_train, X_test, y_train, y_test = train_test_split(data, targets, random_state=1, train_size=0.8)
-
 log_losses= list()
-for column in y_train.iteritems():
+for column in targets.iteritems():
     if len(np.unique(column[1])) > 2:
         # print(np.unique(column[1]))
-        svm.fit(X_train, column[1])
-        prediction = svm.predict(X=X_test)
-        score = log_loss(y_test[column[0]], prediction, labels=[0, 1])
+        svm.fit(data, column[1])
+        prediction = svm.predict(X=test_data)
+        score = log_loss(test_targets[column[0]], prediction, labels=[0, 1])
     else:
-        prediction = np.array([column[1][0] for i in range(len(X_test))])
-        score = log_loss(y_test[column[0]], prediction, labels=[0, 1])
+        prediction = np.array([column[1][0] for i in range(len(test_data))])
+        score = log_loss(test_targets[column[0]], prediction, labels=[0, 1])
     # print(column[0], "Gene Viability PCA score: ", score)
     log_losses.append(score)
 avg_log_loss = sum(log_losses) / len(log_losses)
@@ -55,18 +59,16 @@ print(results_svm)
 
 
 # run model on data with gene pca
-X_train, X_test, y_train, y_test = train_test_split(data_pca, targets, random_state=1, train_size=0.8)
-
 log_losses= list()
-for column in y_train.iteritems():
+for column in targets.iteritems():
     if len(np.unique(column[1])) > 2:
         # print(np.unique(column[1]))
-        svm.fit(X_train, column[1])
-        prediction = svm.predict(X=X_test)
-        score = log_loss(y_test[column[0]], prediction, labels=[0, 1])
+        svm.fit(data_pca, column[1])
+        prediction = svm.predict(X=test_data_pca)
+        score = log_loss(test_targets[column[0]], prediction, labels=[0, 1])
     else:
-        prediction = np.array([column[1][0] for i in range(len(X_test))])
-        score = log_loss(y_test[column[0]], prediction, labels=[0, 1])
+        prediction = np.array([column[1][0] for i in range(len(test_data_pca))])
+        score = log_loss(test_targets[column[0]], prediction, labels=[0, 1])
     # print(column[0], "Gene Viability PCA score: ", score)
     log_losses.append(score)
 avg_log_loss = sum(log_losses) / len(log_losses)
@@ -74,18 +76,16 @@ results_svm.append(avg_log_loss)
 print(results_svm)
 
 # run model on data with gene and viability pca
-X_train, X_test, y_train, y_test = train_test_split(data_pca_all, targets, random_state=1, train_size=0.8)
-
 log_losses= list()
-for column in y_train.iteritems():
+for column in targets.iteritems():
     if len(np.unique(column[1])) > 2:
         # print(np.unique(column[1]))
-        svm.fit(X_train, column[1])
-        prediction = svm.predict(X=X_test)
-        score = log_loss(y_test[column[0]], prediction, labels=[0, 1])
+        svm.fit(data_pca_all, column[1])
+        prediction = svm.predict(X=test_data_pca_all)
+        score = log_loss(test_targets[column[0]], prediction, labels=[0, 1])
     else:
-        prediction = np.array([column[1][0] for i in range(len(X_test))])
-        score = log_loss(y_test[column[0]], prediction, labels=[0, 1])
+        prediction = np.array([column[1][0] for i in range(len(test_data_pca_all))])
+        score = log_loss(test_targets[column[0]], prediction, labels=[0, 1])
     # print(column[0], "Gene Viability PCA score: ", score)
     log_losses.append(score)
 avg_log_loss = sum(log_losses) / len(log_losses)
@@ -94,47 +94,41 @@ print(results_svm)
 
 
 # run model on encoded data
-X_train, X_test, y_train, y_test = train_test_split(data, targets, random_state=1, train_size=0.8)
-clf.fit(X_train,y_train)
-pred = clf.predict(X_test)
-results_rf.append(log_loss(pred, y_test))
+clf.fit(data,targets)
+pred = clf.predict(test_data)
+results_rf.append(log_loss(pred, test_targets))
 print(results_rf)
 
 # run model on data with gene pca
-X_train, X_test, y_train, y_test = train_test_split(data_pca, targets, random_state=1, train_size=0.8)
-clf.fit(X_train,y_train)
-pred = clf.predict(X_test)
-results_rf.append(log_loss(pred, y_test))
+clf.fit(data_pca,targets)
+pred = clf.predict(test_data_pca)
+results_rf.append(log_loss(pred, test_targets))
 print(results_rf)
 
 # run model on data with gene and viability pca
-X_train, X_test, y_train, y_test = train_test_split(data_pca_all, targets, random_state=1, train_size=0.8)
-clf.fit(X_train,y_train)
-pred = clf.predict(X_test)
-results_rf.append(log_loss(pred, y_test))
+clf.fit(data_pca_all,targets)
+pred = clf.predict(test_data_pca_all)
+results_rf.append(log_loss(pred, test_targets))
 print(results_rf)
 
 
 
 # run model on encoded data
-X_train, X_test, y_train, y_test = train_test_split(data, targets, random_state=1, train_size=0.8)
-dum.fit(X_train,y_train)
-pred = dum.predict(X_test)
-results_dum.append(log_loss(pred, y_test))
+dum.fit(data,targets)
+pred = dum.predict(test_data)
+results_dum.append(log_loss(pred, test_targets))
 print(results_dum)
 
 # run model on data with gene pca
-X_train, X_test, y_train, y_test = train_test_split(data_pca, targets, random_state=1, train_size=0.8)
-dum.fit(X_train,y_train)
-pred = dum.predict(X_test)
-results_dum.append(log_loss(pred, y_test))
+dum.fit(data_pca,targets)
+pred = dum.predict(test_data_pca)
+results_dum.append(log_loss(pred, test_targets))
 print(results_dum)
 
 # run model on data with gene and viability pca
-X_train, X_test, y_train, y_test = train_test_split(data_pca_all, targets, random_state=1, train_size=0.8)
-dum.fit(X_train,y_train)
-pred = dum.predict(X_test)
-results_dum.append(log_loss(pred, y_test))
+dum.fit(data_pca_all,targets)
+pred = dum.predict(test_data_pca_all)
+results_dum.append(log_loss(pred, test_targets))
 print(results_dum)
 
 
@@ -142,19 +136,19 @@ print(results_dum)
 plt.bar(labels,results_rf)
 plt.ylabel("Log Loss")
 plt.title("Log Loss of Random Forest Classifier")
-plt.savefig("plots/rf_input_results.png")
+plt.savefig("plots/rf_input_results_test.png")
 plt.close()
 # plot results
 plt.bar(labels,results_svm)
 plt.ylabel("Log Loss")
 plt.title("Log Loss of SVM Classifier")
-plt.savefig("plots/svm_input_results.png")
+plt.savefig("plots/svm_input_results_test.png")
 plt.close()
 # plot results
 plt.bar(labels,results_dum)
 plt.ylabel("Log Loss")
 plt.title("Log Loss of Dummy Classifier")
-plt.savefig("plots/dum_input_results.png")
+plt.savefig("plots/dum_input_results_test.png")
 plt.close()
 
 ##Plot all Results
@@ -168,7 +162,7 @@ rects3 = ax.bar(x + width, results_dum, width, label='Dummy')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Log Loss')
-ax.set_title('Log Loss by Dataset and Model')
+ax.set_title('Log Loss by Dataset and Model (Test Datasets)')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
@@ -191,7 +185,7 @@ autolabel(rects2)
 autolabel(rects3)
 
 plt.ylim(top=20)
-plt.savefig('plots/score_combined.png')
+plt.savefig('plots/score_combined_test_data.png')
 
 
 
